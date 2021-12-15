00b672bee76308580e06d7ceb8a097fc3d13f1f6
Ramish Qamar <95705163+MianRamish@users.noreply.github.com>
2021-12-15 05:37:13 -0800

    Add files via upload

---
+++ 15-12-2021.js
@@ -0,0 +1,24 @@
+var human = {
+    name: "Usama",
+    height: 5.7,
+    eyesColor: "Black"
+}
+var car = {
+    Brand : "BMW",
+    color : "Blue",
+    Power : 5000
+}
+console.log("My name is " + human.name + " & height is  " + human.height + ". My eyes color is " + human.eyesColor);
+
+
+console.log(car.Brand);
+
+var a = "I'm Usama. I'm 21 years old & I'm a web instructor in career institute Millat chowk campus";
+var x = a.concat(" & Jinnah Colony campus");
+console.log(x);
+
+
+
+
+
+
